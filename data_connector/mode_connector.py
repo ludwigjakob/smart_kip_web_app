b2f87a3fb9b94993837c2cfbb52cc27c638efcc3
Ludwig Jakob <ludwig.jakob91@googlemail.com>
2025-11-14 19:12:44 +0000

    Delay in Javascript

--- data_connector/mode_connector.py
+++ data_connector/mode_connector.py
@@ -66,7 +66,7 @@ class ModeConnector(BaseConnector):
     def write_fan_speed(self, speed: int):
         """Speichert die Lüftergeschwindigkeit verzögert in der Datenbank."""
         def delayed_write():
-            time.sleep(2)
+            time.sleep(1)
             conn = mysql.connector.connect(**self.db_config)
             c = conn.cursor()
             c.execute("UPDATE mode SET fan_speed = %s, timestamp = CURRENT_TIMESTAMP WHERE id = 1", (speed,))
